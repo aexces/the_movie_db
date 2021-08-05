5fccd45868148ddce170a4b35770f9cd564ebbad
aexces <aexces@protonmail.com>
2021-08-05 20:57:14 +0530

    initial

---
+++ README.md
@@ -0,0 +1,4 @@
+# The Movie DB
+
+The Movie Database - Flutter
+
---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:the_movie_db/presentation/app_widget.dart';
+
+void main() {
+  runApp(AppWidget());
+}
