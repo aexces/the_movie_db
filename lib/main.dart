3e498066e1b4b72cc163aecb392f2bd50889b0a0
aexces <aexces@protonmail.com>
2022-01-27 22:10:11 +0530

    Splash created

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,7 @@
+import 'package:flutter/cupertino.dart';
 
+import 'presentation/app_widget.dart';
+
+void main() {
+  runApp(const AppWidget());
+}
---
+++ lib/presentation/router/app_router.dart
@@ -0,0 +1,10 @@
+import 'package:auto_route/annotations.dart';
+import 'package:the_movie_db/presentation/pages/splash/splash_page.dart';
+
+@MaterialAutoRouter(
+  replaceInRouteName: "Page,Route",
+  routes: [
+    AutoRoute(page: SplashPage, initial: true),
+  ],
+)
+class $AppRouter {}
